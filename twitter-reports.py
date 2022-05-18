cae6c61b52a72ec6295ef3372b71f7a328567a2c
Death916 <tavn1992@gmail.com>
2022-05-18 04:47:52 -0700

    b4 sockets

--- twitter-reports.py
+++ twitter-reports.py
@@ -1,23 +1,46 @@
 #samdesk-alerts
 
+from asyncore import read
+from gzip import READ
+import re
 from slack_sdk import WebClient
 import os
 import json
 
 def main():
     # Read the environment variables
-    slack_token = os.environ["SLACK_API_TOKEN"]
-    slack_channel = os.environ["SLACK_CHANNEL"]
-    slack_username = os.environ["SLACK_USERNAME"]
+    SLACK_TOKEN = os.environ["SLACK_API_TOKEN"]
+    SLACK_CHANNEL = os.environ["SLACK_CHANNEL"]
+    
     
     # Create a WebClient for sending messages to Slack
-    slack_client = WebClient(token=slack_token)
+    slack_client = WebClient(token=SLACK_TOKEN)
 
-    def read_slack_messages():
-        # Read the messages from the Slack channel
-        response = slack_client.channels_history(channel=slack_channel, count=1)
-        messages = response["messages"]
-        if len(messages) == 0:
-            return None
-        return messages[0]
-    
+    class slack():
+        def __init__(self, slack_client, SLACK_CHANNEL, SLACK_USERNAME):
+            self.slack_client = slack_client
+            self.SLACK_CHANNEL = SLACK_CHANNEL
+            
+
+        
+        def read_messages():
+            # Read the messages from the Slack channel
+            response = slack_client.conversations_list()
+            channel = response["channels"]
+          
+            print(messages)
+
+            return messages[0]
+        
+    slack.read_messages()
+
+
+    class twitter(): 
+        # Read the environment variables
+
+        #search for headlines
+        def search_headlines(self):
+            pass
+
+if __name__ == "__main__":
+    main()
